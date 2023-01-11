281134d60a9fe5880a056a17fc1966d0d101ca6c
Milen <milenmihalkov@gmail.com>
2023-01-11 19:44:21 +0200

    Change page title

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,5 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
-  });
+  setTimeout(()=>{document.title = 'One new message'}, 3000);
 });
